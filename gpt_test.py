78296d30fb0b2e77179aab8f48e493f8747d5fe0
dalong <dalong0514@126.com>
2023-08-18 19:16:04 +0800

    test api

--- gpt_test.py
+++ gpt_test.py
@@ -1,41 +1,21 @@
 import os, time
-
 import openai
-from flask import Flask, redirect, render_template, request, url_for
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
 
-@app.route("/", methods=("GET", "POST"))
-def test_gpt():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="gpt-3.5-turbo",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
+  ]
+)
 
-    result = request.args.get("result")
-    print(result)
+print(completion.choices[0].message.content)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
-
-if __name__ == '__main__':
-    start_time = time.time()
-    test_gpt()
-    end_time = time.time()
-    print('OK!')
-    print('Time Used: ' + str(end_time - start_time) + 's')
+# if __name__ == '__main__':
+#     start_time = time.time()
+#     test_gpt()
+#     end_time = time.time()
+#     print('OK!')
+#     print('Time Used: ' + str(end_time - start_time) + 's')
