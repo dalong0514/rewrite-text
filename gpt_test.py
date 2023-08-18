250e95b951468bc963a6dc132c73a2865b3222a1
dalong <dalong0514@126.com>
2023-08-18 21:48:59 +0800

    test api

--- gpt_test.py
+++ gpt_test.py
@@ -2,35 +2,36 @@ import os, time
 import openai
 
 openai.api_key = "sk-" + "9YonsFAkxw09A3IDkL5qT3BlbkFJ8j80Aco5ecfDRJRYsbce"
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-# messages = [ {"role": "system", "content":
-# 			"You are a intelligent assistant."} ]
-# while True:
-# 	message = input("User : ")
-# 	if message:
-# 		messages.append(
-# 			{"role": "user", "content": message},
-# 		)
-# 		chat = openai.ChatCompletion.create(
-# 			model="gpt-3.5-turbo", messages=messages
-# 		)
-# 	reply = chat.choices[0].message.content
-# 	print(f"ChatGPT: {reply}")
-# 	messages.append({"role": "assistant", "content": reply})
 
-completion = openai.ChatCompletion.create(
-  model="gpt-4",
-  messages=[
-    {"role": "user", "content": "请介绍道金斯《自私的基因》的主要内容"}
-  ]
-)
+def rewrite_text():
 
-print(completion.choices[0].message.content)
+    # 获取当前文件夹的路径
+    current_directory = os.path.dirname(os.path.abspath(__file__))
 
+    # 定义源文件和目标文件的路径
+    origin_path = os.path.join(current_directory, 'origin_text.md')
+    target_path = os.path.join(current_directory, 'target_text.md')
 
-# if __name__ == '__main__':
-#     start_time = time.time()
-#     test_gpt()
-#     end_time = time.time()
-#     print('OK!')
-#     print('Time Used: ' + str(end_time - start_time) + 's')
+    # 读取源文件的内容
+    with open(origin_path, 'r') as origin_file:
+        content = origin_file.read()
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-4",
+    messages=[
+        {"role": "user", "content": content}
+    ]
+    )
+
+    # 将读取的内容写入目标文件
+    with open(target_path, 'w') as target_file:
+        target_file.write(completion.choices[0].message.content)
+
+    # print(completion.choices[0].message.content)
+
+if __name__ == '__main__':
+    start_time = time.time()
+    rewrite_text()
+    end_time = time.time()
+    print('OK!')
+    print('Time Used: ' + str(end_time - start_time) + 's')
